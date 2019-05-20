91e7e60dbfa63a04d6dd8c2c915676585bf5618a
Oleg Kolomiytsev <kolomiytsev.oleg@gmail.com>
2019-05-21 02:00:33 +0300

    Added protogen ex project in order to be able to use find_package with protobuf library.

--- external/patches/protogen-1.0.patches/patch.cmake
+++ external/patches/protogen-1.0.patches/patch.cmake
@@ -1,5 +1,5 @@
 set(PROJECT_ROOT_DIR "${CMAKE_ARGV3}")
-set(PATCH_SOURCE_DIR "${PROJECT_ROOT_DIR}/external/patches/protobuf-gen.patches")
+set(PATCH_SOURCE_DIR "${PROJECT_ROOT_DIR}/external/patches/protogen-1.0.patches")
 
-message(STATUS "Patching protobuf-gen...")
+message(STATUS "Patching protogen...")
 execute_process(COMMAND ${CMAKE_COMMAND} -E copy ${PATCH_SOURCE_DIR}/CMakeLists.txt CMakeLists.txt)
