d800f40c9d8688aecf310bbdaf80ca8768fcc64d
Oleg Kolomiytsev <kolomiytsev.oleg@gmail.com>
2019-06-02 00:47:19 +0300

    protogen without archive.

--- external/patches/protogen-1.0.patches/patch.cmake
+++
@@ -1,5 +0,0 @@
-set(PROJECT_ROOT_DIR "${CMAKE_ARGV3}")
-set(PATCH_SOURCE_DIR "${PROJECT_ROOT_DIR}/external/patches/protogen-1.0.patches")
-
-message(STATUS "Patching protogen...")
-execute_process(COMMAND ${CMAKE_COMMAND} -E copy ${PATCH_SOURCE_DIR}/CMakeLists.txt CMakeLists.txt)
